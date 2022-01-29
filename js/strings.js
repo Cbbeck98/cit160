7a540afed0ae31d41c7d87bfbb302c8d0bf3a7f4
Cbbeck98 <bec20031@byui.edu>
2022-01-29 15:32:53 -0700

    re-setup

--- js/strings.js
+++ js/strings.js
@@ -1,11 +1,15 @@
 document.querySelector('#copyright').textContent = new Date().getFullYear();
 
-function ___() {
+function myFunction () {
 	// INPUT
-	
+	let first = document.getElementById('firstname').value;
+	let fruit = document.getElementById('fruit').value;
+	let aml1 = document.getElementById('animal1').value;
+	let aml2 = document.getElementById('animal1').value;
+
 	// PROCESSING
-	 
+
 	
 	//OUTPUT - Example output: "And it came to pass that the people of ___ did till the land, and raise all manner of ___, and flocks of herds, and flocks of all manner of ___ of every kind, and ___."
-	 
+	let outputString = '"And it came to pass that the people of ${first} did till the land, and raise all manner of ${fruit}, and flocks of herds, and flocks of all manner of ${aml1} of every kind, and ${aml2."'
 }
